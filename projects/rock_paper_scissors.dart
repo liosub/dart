4ddd069b18075f4cd0707f7c99bd19f918ee7e80
liosub <mlio64565@gmail.com>
2021-05-04 01:24:19 +0100

    final game

--- projects/rock_paper_scissors.dart
+++ projects/rock_paper_scissors.dart
@@ -1,3 +1,42 @@
+import 'dart:io';
+import 'dart:math';
+
+enum Move { rock, paper, scissors }
 void main() {
-  print('hello world');
+  final ran = Random();
+  dynamic flag = true;
+  stdout.write("Enter Your name Please");
+  final playername = stdin.readLineSync();
+  while (flag == true) {
+    stdout.write("Rock , Paper or Scissors ? (r/p/s): ");
+    final input = stdin.readLineSync();
+    if (input == 'r' || input == 'p' || input == 's') {
+      var playerMove;
+      if (input == 'r') {
+        playerMove = Move.rock;
+      } else if (input == 'p') {
+        playerMove = Move.paper;
+      } else {
+        playerMove = Move.scissors;
+      }
+      final random = ran.nextInt(3);
+      final move = Move.values[random];
+
+      if (playerMove == move) {
+        print('it\'s draw');
+      } else if ((playerMove == Move.rock && move == Move.scissors) ||
+          (playerMove == Move.paper && move == Move.rock) ||
+          (playerMove == Move.scissors && move == Move.paper)) {
+        print('You win $playername');
+      } else {
+        print('PC beat you ,good job hamasa');
+      }
+    } else if (input == 'q') {
+      flag = false;
+      print('Game Over');
+      break;
+    } else {
+      print('Invalid input');
+    }
+  }
 }
